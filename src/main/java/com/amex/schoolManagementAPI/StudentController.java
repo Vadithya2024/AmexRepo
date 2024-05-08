f7d4ddf68483618ceeccdf5748d89a36e4bf7c98
vadithya2024 <vadithya.na@gmail.com>
2024-05-07 19:52:31 -0500

    updated json deserialization

--- src/main/java/com/amex/schoolManagementAPI/StudentController.java
+++ src/main/java/com/amex/schoolManagementAPI/StudentController.java
@@ -1,5 +1,7 @@
 package com.amex.schoolManagementAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +13,11 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @PostMapping
     public Student addStudent(@RequestBody Student student) {
+        logger.debug("Received student: {}", student);
         return studentService.addStudent(student);
     }
 
